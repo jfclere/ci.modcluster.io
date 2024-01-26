5def306218c004b5273498bedc86dec03db6e42b
Jean-Frederic Clere <jfclere@gmail.com>
2024-01-26 06:03:37 -0800

    In fact openssl must be in the same location than apr/apr-util/httpd etc.

--- windows/openssl/build.bat
+++ windows/openssl/build.bat
@@ -4,10 +4,7 @@ REM This script builds OpenSSL
 
 set WORKSPACE=c:\Tools\HTTPD
 set SOURCES=c:\Tools\SOURCES
-set MYTARGET="c:/Program Files (x86)"
-
-mkdir %WORKSPACE%\target\64
-mkdir %WORKSPACE%\openssl\build-64
+set MYTARGET="c:/Program Files (x86)/APR"
 
 PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin;C:\Program Files\NASM
 
@@ -15,7 +12,10 @@ PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\
 call vcvars64
 
 cd %SOURCES%\openssl
-perl Configure
+perl Configure --prefix=%MYTARGET%
 nmake
-nmake test
 nmake install
+
+# $Env:HARNESS_VERBOSE = 'yes'
+# nmake test TESTS=test_quicapi HARNESS_VERBOSE=yes
+# REM nmake install

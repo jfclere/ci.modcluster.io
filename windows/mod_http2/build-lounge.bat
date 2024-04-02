2f09bad9a24f790a5f922a4876b7708a5c1bb9ba
Jean-Frederic Clere <jfclere@gmail.com>
2024-04-02 07:49:10 -0700

    Add apache-lounge build examples.

---
+++ windows/mod_http2/build-lounge.bat
@@ -0,0 +1,34 @@
+REM @author: Michal Karm Babacek <karm@fedoraproject.org>
+
+REM This script is used to build and package mod_proxy_cluster modules for httpd trunk
+
+set WORKSPACE=c:\Tools\HTTPD
+set SOURCES=c:\Tools\SOURCES
+set MYTARGET="c:/Tools/Apache24"
+
+PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
+
+call vcvars64
+
+REM CMake workspace
+del /s /f /q %WORKSPACE%\cmakebuild
+mkdir %WORKSPACE%\cmakebuild
+pushd %WORKSPACE%\cmakebuild
+
+SET HTTPD_DEV_HOME_POSSIX=%HTTPD_DEV_HOME:\=/%
+
+cmake -G "Visual Studio 17 2022" ^
+-DAPR_LIBRARY=%MYTARGET%/lib/libapr-1.lib ^
+-DAPR_INCLUDE_DIR=%MYTARGET%/include/ ^
+-DAPACHE_INCLUDE_DIR=%MYTARGET%/include/ ^
+-DAPRUTIL_LIBRARY=%MYTARGET%/lib/libaprutil-1.lib ^
+-DAPRUTIL_INCLUDE_DIR=%MYTARGET%/include/ ^
+-DAPACHE_LIBRARY=%MYTARGET%/lib/libhttpd.lib ^
+-DPROXY_LIBRARY=%MYTARGET%/lib/mod_proxy.lib ^
+-DNGHTTP2_LIBRARIES=%MYTARGET%/lib/nghttp2.lib ^
+%SOURCES%/mod_h2/
+
+cd c:\Tools\HTTPD\cmakebuild
+MSBuild ALL_BUILD.vcxproj -t:build -p:Configuration=Release
+
+copy  %WORKSPACE%\cmakebuild\Release\*.so %MYTARGET%\modules\
---
+++ windows/nghttp2/build-lounge.bat
@@ -0,0 +1,33 @@
+REM @author: Michal Karm Babacek <karm@fedoraproject.org>
+REM This script builds nghttp2
+
+set WORKSPACE=c:\Tools\HTTPD
+set SOURCES=c:\Tools\SOURCES
+set MYTARGET="c:/Tools/Apache24"
+
+REM Build environment
+PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
+
+call vcvars64
+
+del /s /f /q %WORKSPACE%\cmakebuild
+mkdir %WORKSPACE%\cmakebuild
+pushd %WORKSPACE%\cmakebuild
+
+REM Note that some attributes cannot handle backslashes...
+SET WORKSPACEPOSSIX=%WORKSPACE:\=/%
+
+cmake -G "Visual Studio 17 2022" ^
+%SOURCES%/nghttp2
+
+cd c:\Tools\HTTPD\cmakebuild
+MSBuild ALL_BUILD.vcxproj -t:build -p:Configuration=Release
+
+REM install part
+copy lib\Release\* %MYTARGET%\lib
+del /s /f /q  %MYTARGET%\include\nghttp2
+mkdir  %MYTARGET%\include\nghttp2
+copy lib\includes\nghttp2\*  %MYTARGET%\include\nghttp2
+copy %SOURCES%\nghttp2\lib\includes\nghttp2\nghttp2.h  %MYTARGET%\include\nghttp2
+
+popd

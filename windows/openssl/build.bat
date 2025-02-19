e5273a94d6c2bd049575fb53a1f27d6c890e25c6
Jean-Frederic Clere <jfclere@gmail.com>
2025-02-19 18:13:30 +0100

    Add what is needed for win7

--- windows/openssl/build.bat
+++ windows/openssl/build.bat
@@ -6,16 +6,27 @@ set WORKSPACE=c:\Tools\HTTPD
 set SOURCES=c:\Tools\SOURCES
 set MYTARGET="c:/Program Files (x86)/APR"
 
+ver | findstr "10.0" &&  goto new
+
+PATH=C:\perl\bin;C:\Tools\nasm-2.16.03;C:\Tools\cmake-3.22.6-windows-x86_64/bin;%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\amd64;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+goto next
+
+:new
+
 PATH=%PATH%;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin;C:\Program Files\NASM
 
+:next
 
 call vcvars64
+
+cl
 
 cd %SOURCES%\openssl
 perl Configure --prefix=%MYTARGET%
 nmake
 nmake install
 
-# $Env:HARNESS_VERBOSE = 'yes'
-# nmake test TESTS=test_quicapi HARNESS_VERBOSE=yes
-# REM nmake install
+REM $Env:HARNESS_VERBOSE = 'yes'
+REM  nmake test TESTS=test_quicapi HARNESS_VERBOSE=yes
+REM nmake install
